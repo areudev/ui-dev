e0d94add1509fb42ce2234077206fb7a0488504f
Christos Kalivas <chrkalivas@gmail.com>
2023-06-28 21:06:23 +0300

    counter

---
+++ src/demos/counter/index.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { Frame } from '../../components/frame'
+import { Button } from '../../components/button'
+
+type CounterProps = {
+	initialValue?: number
+}
+
+const Counter = ({ initialValue = 0 }: CounterProps) => {
+	const [count, setCount] = useState(initialValue)
+
+	return (
+		<Frame className="text-center ">
+			<header>
+				<h1>Complex Counter</h1>
+			</header>
+			<div className="text-center text-8xl" data-testid="current-count">
+				{count}
+			</div>
+			<div className="flex gap-4 justify-center">
+				<Button variant="destructive" onClick={() => setCount(0)}>
+					Reset
+				</Button>
+				<Button onClick={() => setCount(c => c + 1)}>Increment</Button>
+			</div>
+		</Frame>
+	)
+}
+
+export default Counter
