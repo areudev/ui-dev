1bb9ccbc08a2618a46be9249c84e9dc75ba406e2
Christos Kalivas <chrkalivas@gmail.com>
2023-06-28 23:30:22 +0300

    calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Calendar } from './components/calendar'
 import Counter from './demos/counter'
 
 function App() {
 	return (
 		<>
 			<Counter />
+			<Calendar onDayClick={(d, m, y) => console.log(d, m, y)} />
 		</>
 	)
 }
