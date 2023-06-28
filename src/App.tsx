491db359862eb4f8251c4b44b18917ff9627103a
Christos Kalivas <chrkalivas@gmail.com>
2023-06-28 18:41:35 +0300

    input checkbox label

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import {Button} from './components/button'
+import {Input} from './components/input'
+import {Label} from './components/label'
+import {Checkbox} from './components/checkbox'
 
 function App() {
 	return (
-		<div className="bg-background text-foreground container">
-			<div className="flex flex-col align-center justify-center">
-				<img src={reactLogo} alt="react" className="h-12 mr-2" />
-				<img src={viteLogo} alt="vite" className="h-12" />
-				<div>
-					<Button>Hello Vite + React!</Button>
-				</div>
+		<div className="container flex flex-col items-center justify-center gap-2">
+			<h1 className="text-3xl">Yo world</h1>
+			<div>
+				<Button>Hi Mom</Button>
+			</div>
+			<div className="flex flex-col gap-2">
+				<Label>Label</Label>
+				<Input placeholder="Yo yoyo yo yo" />
+			</div>
+			<div className="flex gap-2">
+				<Checkbox />
+				<Label>Checkbox</Label>
 			</div>
 		</div>
 	)
