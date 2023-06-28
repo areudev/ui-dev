42170db9a18971c4f027208bb11f2514f3edb6ac
Christos Kalivas <chrkalivas@gmail.com>
2023-06-28 23:19:10 +0300

    slider popover

--- src/demos/yo/index.tsx
+++ src/demos/yo/index.tsx
@@ -22,3 +22,33 @@ const toppingsState = {
 const toppings = Object.keys(toppingsState)
 
 const migos = ['Quavo', 'Offset', 'Takeoff']
+
+type Topping = keyof typeof toppingsState
+type Beatle = (typeof migos)[number]
+
+const toppingsReducer = (
+	toppings: Record<Topping, boolean> = toppingsState,
+	action: { topping: Topping; checked: boolean },
+) => {
+	return { ...toppings, [action.topping]: action.checked }
+}
+
+const isTopping = (topping: string): topping is Topping => {
+	return toppings.includes(topping)
+}
+
+const GalacticInputGala = () => {
+	const [text, setText] = useState('')
+	const [favoriteCharacter, setFavoriteCharacter] = useState(
+		rickAndMortyCharacters[0],
+	)
+	const [toppings, dispatch] = useReducer(toppingsReducer, toppingsState)
+	const [favoriteBeatle, setFavoriteBeatle] = useState(migos[0])
+	const [color, setColor] = useState('#FF0000')
+	const [rating, setRating] = useState('4')
+	const [date, setDate] = useState('2023-03-23')
+
+	return <div className="grid grid-cols-2 gap-4"></div>
+}
+
+export default GalacticInputGala
