3afb8b94750e36388c8a94ba3c647158ecc3ec29
Christos Kalivas <chrkalivas@gmail.com>
2023-06-28 18:08:05 +0300

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Button from './components/button'
+import {Button} from './components/button'
 
 function App() {
 	return (
-		<div className="dark bg-background text-foreground container">
+		<div className="bg-background text-foreground container">
 			<div className="flex flex-col align-center justify-center">
 				<img src={reactLogo} alt="react" className="h-12 mr-2" />
 				<img src={viteLogo} alt="vite" className="h-12" />
