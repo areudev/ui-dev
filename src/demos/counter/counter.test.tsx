1ea5740671034570d15519b25c375801fc769404
Christos Kalivas <chrkalivas@gmail.com>
2023-07-04 12:45:41 +0300

    counter tests config set

--- src/demos/counter/counter.test.tsx
+++ src/demos/counter/counter.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Counter from '.'
 import { test } from 'vitest'
 
@@ -9,4 +9,18 @@ test('it bodies', () => {
 test('it should render the component', () => {
 	render(<Counter />)
 	screen.debug(document.body)
+})
+
+test('it should inccrement the counter', async () => {
+	render(<Counter />)
+	const currentCount = screen.getByTestId('current-count')
+	screen.debug(currentCount)
+	fireEvent.click(screen.getByRole('button', { name: /increment/i }))
+	fireEvent.click(screen.getByRole('button', { name: /increment/i }))
+	// expect(currentCount.textContent).toBe('0')
+	expect(currentCount).toHaveTextContent('2')
+
+	fireEvent.click(screen.getByRole('button', { name: /reset/i }))
+	expect(currentCount).toHaveTextContent('0')
+	// expect(currentCount)('0')
 })
