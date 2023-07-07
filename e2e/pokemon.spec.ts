b2daca322844c93faa4712cc53ebde39cee188d3
Christos Kalivas <chrkalivas@gmail.com>
2023-07-08 00:27:08 +0300

    playwright network requests

--- e2e/pokemon.spec.ts
+++ e2e/pokemon.spec.ts
@@ -2,8 +2,10 @@ import { test, expect } from '@playwright/test'
 
 test('it has an input for pokemons', async ({ page }) => {
 	await page.goto('http://localhost:5173/pokemon')
-	const input = page.getByPlaceholder('Search for a pokemon')
-	await input.type('pikachu')
+	const searchInput = page.getByPlaceholder('Search for a pokemon')
+	await searchInput.type('pikachu')
 	await page.waitForSelector('text=pikachu')
-	const pikachuButton = page.getByRole('button', { name: 'pikachu' })
+	const pikachuLink = page.getByRole('button', { name: 'pikachu' })
+	await pikachuLink.click()
+	expect(page.url()).toBe('http://localhost:5173/pokemon/25')
 })
